ad027dd6dab290f32b87bc2a9a80150d6774277b
Ihor Kazakov <s29216@pjwstk.edu.pl>
2026-06-23 18:17:22 +0200

    Add console input handling for numbers

--- StatisticsApp/Program.cs
+++ StatisticsApp/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using StatisticsApp;
+
+Console.WriteLine("Enter numbers separated by spaces:");
+string? input = Console.ReadLine();
+
+int[] numbers = input!
+    .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+    .Select(int.Parse)
+    .ToArray();
+
+Console.WriteLine($"Sum: {StatisticsHelper.Sum(numbers)}");
